dwasint/Vanderlin

The Title Update (#5451)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->

## About The Pull Request

<!-- Describe The Pull Request. Please be sure every change is
documented or this can delay review and even discourage maintainers from
merging your PR! -->
Moves all Titles into their own honorary variable instead of their real
name. Titles are added to names when examined or looked at but are not
their true name. This means to make King John and outlaw, you would
actually make "John" an outlaw.
Some roles have received titles as well. Most royals get one now, as
well as many others.

## Why It's Good For The Game

<!-- Argue for the merits of your changes and how they benefit the game,
especially if they are controversial and/or far reaching. If you can't
actually explain WHY what you are doing will improve the game, then it
probably isn't good for the game in the first place. -->
Clarity, and better roleplay. Acknowledgement of titles.

## Changelog

<!-- If your PR modifies aspects of the game that can be concretely
observed by players or admins you should add a changelog. If your change
does NOT meet this description, remove this section. Please note that
maintainers freely reserve the right to remove and add tags should they
deem it appropriate. You can attempt to finagle the system all you want,
but it's best to shoot for clear communication right off the bat. -->

<!-- !! Do not add whitespace in-between the entries, do not change the
tags and do not leave the tags without any entries. -->
:cl:
add: Honorary Titles
/:cl:

<!-- Both :cl:'s are required for the changelog to work! You can put
your name to the right of the first :cl: if you want to overwrite your
GitHub username as author ingame. -->
<!-- You can use multiple of the same prefix (they're only used for the
icon ingame) and delete the unneeded ones. Despite some of the tags,
changelogs should generally represent how a player might be affected by
the chang

code/modules/antagonists/villain/assassin.dm
@@ -76,3 +76,13 @@
 		to_chat(world, "<span class='redtext'>The [name] [owner.name] has FAILED!</span>")
 		if(owner?.current)
 			owner.current.playsound_local(get_turf(owner.current), 'sound/misc/fail.ogg', 100, FALSE, pressure_affected = FALSE)
+
+/datum/antagonist/assassin/examine_target(mob/living/carbon/examiner, mob/living/examined, list/P, list/examine_contents)
+	. = ..()
+	if(!istype(examiner) || !istype(examined))
+		return
+	if(examined.has_quirk(/datum/quirk/vice/hunted) || HAS_TRAIT(src, TRAIT_ZIZOID_HUNTED))
+		for(var/obj/item/I in examiner.get_all_gear())
+			if(istype(I, /obj/item/weapon/knife/dagger/steel/profane))
+				LAZYADDASSOCLIST(examine_contents, EXAMINE_SECT_PREGEAR, "profane dagger whispers, [span_danger("\"That's [examined.real_name]! Strike their heart!\"")]")
+				break

SurmanDJ/Rivermist-Hollow-Vanderl-SPRITES

code/modules/antagonists/villain/assassin.dm
@@ -76,3 +76,13 @@
 		to_chat(world, "<span class='redtext'>The [name] [owner.name] has FAILED!</span>")
 		if(owner?.current)
 			owner.current.playsound_local(get_turf(owner.current), 'sound/misc/fail.ogg', 100, FALSE, pressure_affected = FALSE)
+
+/datum/antagonist/assassin/examine_target(mob/living/carbon/examiner, mob/living/examined, list/P, list/examine_contents)
+	. = ..()
+	if(!istype(examiner) || !istype(examined))
+		return
+	if(examined.has_quirk(/datum/quirk/vice/wanted) || HAS_TRAIT(examined, TRAIT_ZIZOID_HUNTED))
+		for(var/obj/item/I in examiner.get_all_gear())
+			if(istype(I, /obj/item/weapon/knife/dagger/steel/profane))
+				LAZYADDASSOCLIST(examine_contents, EXAMINE_SECT_PREGEAR, "profane dagger whispers, [span_danger("\"That's [examined.real_name]! Strike their heart!\"")]")
+				break